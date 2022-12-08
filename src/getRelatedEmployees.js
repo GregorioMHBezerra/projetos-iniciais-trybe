8946885b44539adfdef12ad999cf4f922728770d
Gregório de M H Bezerra <gregoriodmhbezerra@gmail.com>
2022-12-08 19:03:37 -0300

    Correção do requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -3,24 +3,16 @@ const { employees } = require('../data/zoo_data');
 const isManager = (id) => employees.some((employee) =>
   employee.managers.includes(id));
 
-// const verifyManager = (iD) => {
-//   if (isManager(iD) === false) {
-//     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
-//   }
-// };
-
-const getRelatedEmployees = (managerId) => {
-  // try {
-    // verifyManager(managerId);
+const verifyManager = (iD) => {
   if (isManager(iD) === false) {
     throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
-  const workst = employees.filter((employee) => employee.managers.includes(managerId));
-  return workst.map((works) => `${works.firstName} ${works.lastName}`);
-  // } catch (error) {
-  //   return error.message;
-  // }
+  }
 };
 
-// console.log(getRelatedEmployees('0e7b460e-acf4-4e17-bcb3-ee472265d83'));
+const getRelatedEmployees = (managerId) => {
+  verifyManager(managerId);
+  const workst = employees.filter((employee) => employee.managers.includes(managerId));
+  return workst.map((works) => `${works.firstName} ${works.lastName}`);
+};
 
 module.exports = { isManager, getRelatedEmployees };
