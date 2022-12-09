f8e8ee0528a0de8f5e73749c090a8d6388090c38
Gregório de M H Bezerra <gregoriodmhbezerra@gmail.com>
2022-12-09 19:00:44 -0300

    Adiciona o requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,28 @@
-const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
-const countEntrants = (entrants) => {
-  // seu código aqui
-};
+const countEntrants = (entrants) => entrants.reduce((acc, entrant) => {
+  if (entrant.age < 18) {
+    acc.child += 1;
+    // return acc;
+  } else if (entrant.age < 50) {
+    acc.adult += 1;
+    // return acc;
+  } else {
+    acc.senior += 1;
+  }
+  return acc;
+}, { child: 0, adult: 0, senior: 0 });
 
 const calculateEntry = (entrants) => {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length <= 0) {
+    return 0;
+  }
+  const visitantes = countEntrants(entrants); // retorna um objeto
+  const teste = Object.keys(visitantes);
+  return teste.reduce((acc, curr, index) => {
+    const nameAux = teste[index];
+    return acc + visitantes[nameAux] * prices[nameAux];
+  }, 0);
 };
 
 module.exports = { calculateEntry, countEntrants };
