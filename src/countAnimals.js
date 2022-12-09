aa4d856843492bc301bdb1597ecf8ce31305b5b7
Gregório de M H Bezerra <gregoriodmhbezerra@gmail.com>
2022-12-09 17:39:01 -0300

    adiciona o requisito 5 - voltar para estudar, pois foi difícil

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,9 +1,19 @@
 const { species } = require('../data/zoo_data');
 
 const countAnimals = (animal) => {
-  species.reduce((acc, specie) => {
-    
-  }, {});
+  if (!animal) {
+    return species.reduce((acc, specie) => {
+      acc[specie.name] = specie.residents.length;
+      return acc;
+    }, {});
+  }
+  const searchAnimal = species.find((specie) => specie.name === animal.species);
+  if (animal.species && !animal.sex) {
+    return searchAnimal.residents.length;
+  }
+  return searchAnimal.residents.filter((ind) => ind.sex === animal.sex).length;
 };
+
+console.log(countAnimals({ species: 'penguins', sex: 'male' }));
 
 module.exports = countAnimals;
