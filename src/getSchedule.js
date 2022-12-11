bc78e538a869c17663c1bf8cb0c4514ca93c8955
Gregório de M H Bezerra <gregoriodmhbezerra@gmail.com>
2022-12-11 14:52:11 -0300

    Adiciona requisito 9

--- src/getSchedule.js
+++ src/getSchedule.js
@@ -1,7 +1,39 @@
-const data = require('../data/zoo_data');
+const { species, hours } = require('../data/zoo_data');
+
+const allSchedule = () => Object.entries(hours).reduce((acc, day) => {
+  const exhibition = species.filter(((specie) => specie.availability.includes(day[0])));
+  if (day[0] !== 'Monday') {
+    acc[day[0]] = {
+      officeHour: `Open from ${day[1].open}am until ${day[1].close}pm`,
+      exhibition: exhibition.map((specie) => specie.name) };
+  } else if (day[0] === 'Monday') {
+    acc[day[0]] = {
+      officeHour: 'CLOSED',
+      exhibition: 'The zoo will be closed!' };
+  }
+  return acc;
+}, {});
 
 const getSchedule = (scheduleTarget) => {
-  // seu código aqui
+  const days = Object.keys(hours);
+  const animals = species.reduce((acc, specie) => {
+    acc.push(specie.name);
+    return acc;
+  }, []);
+  if (animals.includes(scheduleTarget)) {
+    return species.find((specie) => specie.name === scheduleTarget).availability;
+  }
+  if (days.includes(scheduleTarget)) {
+    return Object.entries(allSchedule()).find((day) =>
+      day[0] === scheduleTarget).reduce((acc, day, index, array) => {
+      const [dayWeek, workHours] = array;
+      acc[dayWeek] = workHours;
+      return acc;
+    }, {});
+  }
+  return allSchedule();
 };
+
+console.log(getSchedule());
 
 module.exports = getSchedule;
